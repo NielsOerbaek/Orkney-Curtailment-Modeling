674d15472aec1cf524c61b3ea25704b202d528bd
Niels Ørbæk Christensen <NielsOerbaek@gmail.com>
2019-05-24 14:20:55 +0200

    Cleaning house for hand-in

--- getAndStorePrediction.py
+++
@@ -1,9 +0,0 @@
-import time
-import predict as p
-
-starttime = time.time()
-wait_secs = 10*60.0
-
-while True:
-    p.getAndStorePrediction()
-    time.sleep(wait_secs - ((time.time() - starttime) % wait_secs))
