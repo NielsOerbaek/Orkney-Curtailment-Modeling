ee5ead7721daa82048500ab9d62c23bf17a1459d
Niels Ørbæk Christensen <NielsOerbaek@gmail.com>
2019-05-24 14:15:14 +0200

    Save this branch

--- getAndStorePrediction.py
+++ getAndStorePrediction.py
@@ -3,7 +3,7 @@ import predict as p
 
 starttime = time.time()
 wait_secs = 10*60.0
-
+ 
 while True:
     p.getAndStorePrediction()
     time.sleep(wait_secs - ((time.time() - starttime) % wait_secs))
